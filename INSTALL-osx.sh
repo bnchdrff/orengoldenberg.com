e6d2abbfd2854bc43ceb1677629ba0ec58dc153e
Antonio <antoniomanzari@gmail.com>
2013-12-06 14:47:23 -0500

    Add updated Install shell script

--- INSTALL-osx.sh
+++ INSTALL-osx.sh
@@ -1,6 +1,14 @@
+#if necessary install homebrew
+#ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)" 
+
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-nano .profile #add nvm command see docs
+#add nvm command see docs, add "source ~/.nvm/nvm.sh" to .profile file
+#OR you can append that code to the .profile file using "echo 'source ~/.nvm/nvm.sh' >> ~/.profile"
+
+nano .profile
+
+#restart shell for next step
 
 nvm install v0.10.22
 
@@ -26,10 +34,11 @@ rbenv rehash
 
 #all below is related to local project
 
-#remember you can reclone the project if necessary! 
+#remember you can reclone the project if you need to start over!
 
 
-rbenv shell 1.9.3-p448
+
+rbenv shell 1.9.3-p448 #this allows your to use compass
 
 cd orengoldenberg.com
 
@@ -39,3 +48,8 @@ rbenv rehash
 
 npm install
 
+#to run project
+
+cd orengoldenberg.com
+
+grunt server
