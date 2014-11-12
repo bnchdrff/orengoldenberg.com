62edcb7fbb7f674e3068dd5a6562f37bad70c825
Benjamin Chodoroff <ben@theworkdept.com>
2014-11-12 11:48:12 -0500

    offer global router to tricks

--- app/entry.js
+++ app/entry.js
@@ -1,9 +1,10 @@
 var Router = require('./router'),
     routes = require('./routes'),
-    router = new Router(routes);
-    Tricks = require('./tricks'),
-    tricks = new Tricks(window);
+    router = new Router(routes),
+    Tricks = require('./tricks');
 
 window.router = router;
 
 router.start(window.allVideos);
+
+var tricks = new Tricks(window);
