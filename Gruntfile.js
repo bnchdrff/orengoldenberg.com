80430179b09a8986c6f26d51e582dc70ea020057
Benjamin Chodoroff <ben@theworkdept.com>
2015-01-25 23:00:47 -0500

    prod prod

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
     browserify: {
       prod: {
         options: {
-          debug: true,
+          debug: false,
           transform: ['hbsfy'],
           aliasMappings: [
             {
