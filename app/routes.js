e32dc149f787139b33d96eee6158008f12caf6df
Benjamin Chodoroff <ben@theworkdept.com>
2015-02-03 11:06:27 -0500

    remove consolelogs

--- app/routes.js
+++ app/routes.js
@@ -4,12 +4,10 @@ var apiClient = require('./api_client'),
 
 module.exports = function(match) {
   match('/', function(callback) {
-    console.log('index hit');
     callback(null, 'index');
   });
 
   match('/videos', function(callback) {
-    console.log('videos hit');
     apiClient.get('/videos.json', function(err, res) {
       if (err) {
         return callback(err);
@@ -20,7 +18,6 @@ module.exports = function(match) {
   });
 
   match('/videos/:id', function(id, callback) {
-    console.log('videos/' + id + ' hit');
     apiClient.get('/videos/' + id + '.json', function(err, res) {
       if (err) {
         return callback(err);
@@ -31,7 +28,6 @@ module.exports = function(match) {
   });
 
   match('/videos-tagged/(.*)', function(tag, callback) {
-    console.log('videos-tagged/' + tag + ' hit');
     apiClient.get('/videos-tagged/' + tag + '.json', function(err, res) {
       if (err) {
         return callback(err);
@@ -45,19 +41,12 @@ module.exports = function(match) {
   });
 
   match('/pages/:page', function(page, callback) {
-    console.log('pages/' + page + ' hit');
     apiClient.get('/pages/' + page + '.json', function(err, res) {
       if (err) {
         return callback(err);
       }
-      console.log('res'); console.log(res.body.meta);
       var page = res.body;
       callback(null, 'page', page);
     });
   });
-
-  match('/video-thumb/:id', function(id, callback) {
-    console.log('video-thumb/' + id + ' hit');
-  });
-
 };
